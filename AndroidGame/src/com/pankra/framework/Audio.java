855862429832b697233e01c9cdcb4ed696ff8950
SPankratov <Sergey.Pankratov@aplana.com>
2015-02-12 10:16:42 +0500

    Unit 4-5 http://www.kilobolt.com/day-5-the-android-game-framework-part-i.html
    initial

---
+++ AndroidGame/src/com/pankra/framework/Audio.java
@@ -0,0 +1,7 @@
+package com.pankra.framework;
+
+public interface Audio {
+	public Music createMusic(String file);
+
+	public Sound createSound(String file);
+}
---
+++ AndroidGame/src/com/pankra/framework/Sound.java
@@ -0,0 +1,7 @@
+package com.pankra.framework;
+
+public interface Sound {
+	public void play(float volume);
+
+	public void dispose();
+}
